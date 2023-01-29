394fe1c32a4e9501a352d9498bfd08d01d1cf512
Andrey <andreytelushko@gmail.com>
2023-01-29 09:36:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork9
+{
+    internal class Program
+    {  
+        static void Main()
+            {
+                Console.WriteLine("Введите число до которого вывести натуральные числа: ");
+                int inp = int.Parse(Console.ReadLine());
+                Console.Clear();
+                NumRowButReversed(inp);
+                Console.ReadKey();
+            }
+
+            public static int NumRowButReversed(int inp)
+            {
+                if (inp == 0) return 1;
+                else Console.Write(inp-- + " ");
+                NumRowButReversed(inp);
+                return -1;
+            }
+        
+    }
+}
