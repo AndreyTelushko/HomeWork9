262d0350a3b9f217076ce61ec770451b48c96eca
Andrey <andreytelushko@gmail.com>
2023-01-29 09:38:13 +0300

    Lesson66

--- HomeWork9/Program.cs
+++ HomeWork9/Program.cs
@@ -7,23 +7,36 @@ using System.Threading.Tasks;
 namespace HomeWork9
 {
     internal class Program
-    {  
+    {
         static void Main()
-            {
-                Console.WriteLine("Введите число до которого вывести натуральные числа: ");
-                int inp = int.Parse(Console.ReadLine());
-                Console.Clear();
-                NumRowButReversed(inp);
-                Console.ReadKey();
-            }
+        {
+            Console.WriteLine("Введите диапазон: ");
+            Console.Write("От: ");
+            int inp1 = int.Parse(Console.ReadLine());
+            Console.Write("До: ");
+            int inp2 = int.Parse(Console.ReadLine());
+            int cntr = 0;       // Инициализация счетчика
+            Console.Clear();
 
-            public static int NumRowButReversed(int inp)
+            Console.WriteLine($"Сумма диапазона: {SumOfRange(inp1, inp2, ref cntr)}");
+            Console.ReadKey();
+        }
+
+        public static int SumOfRange(int inp1, int inp2, ref int cntr)
+        {
+            // Исключение ошибки с первым числом больше второго
+            int temp = 0;
+            if (inp1 > inp2)
             {
-                if (inp == 0) return 1;
-                else Console.Write(inp-- + " ");
-                NumRowButReversed(inp);
-                return -1;
+                temp = inp1;
+                inp1 = inp2;
+                inp2 = temp;
             }
-         
+            cntr += inp1; // Счетчик
+
+            if (inp1 == inp2) return cntr;
+            else SumOfRange(inp1 + 1, inp2, ref cntr); // Точка входа в рекурсию
+            return cntr;
+        }
     }
 }
